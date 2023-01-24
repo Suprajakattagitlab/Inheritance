adf28b02f6f44638879ecb297a047738e4bd2bc6
SuprajaKatta9 <supraja.katta@klingelnberg.com>
2023-01-24 14:42:13 +0530

    Commiting array

---
+++ Array-Collections/Array-Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using Array_Collections;
+namespace Array_Collections
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Train_Schedule t1 = new Train_Schedule(23, "Solapur", "Pune");
+            Train_Schedule t2 = new Train_Schedule(24, "Pune", "Solapur");
+            Train_Schedule [] t=new Train_Schedule[2];
+            t[0] = t1;
+            t[1] = t1;
+
+            int[] a = new int[3];
+            foreach(Train_Schedule train in t)
+            {
+                Console.WriteLine(train.to);
+            }
+
+
+        }
+    }
+}
---
+++ Array-Collections/Array-Collections/Tarin_Schedule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Collections
+{
+    internal class Train_Schedule
+
+    {
+        
+            int PNR;
+            public string to;
+            public string from;
+            public Train_Schedule(int PNR, string to, string from)
+            {
+            //  DateTime d = new DateTime();
+            this. PNR = PNR;
+                this.to = to;
+                this.from = from;
+            }
+            public override string ToString() => $"{PNR}:{from}->{to}";
+
+
+        
+
+    }
+}
